fbefea2de622fe23c90f36f7e25de4129a109298
divyamds <55553324+divyamds@users.noreply.github.com>
2025-08-03 14:04:53 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,8 +18,10 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        Vite::prefetch(concurrency: 3);
+   public function boot()
+{
+    if (config('app.env') === 'production') {
+        URL::forceScheme('https');
     }
+}
 }
