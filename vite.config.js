5dfe9086b56a6001585d976205cc10ce3c8ce930
divyamds <55553324+divyamds@users.noreply.github.com>
2025-08-03 12:17:47 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    base: process.env.ASSET_URL + '/build/',
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: ['resources/js/app.jsx'],
             refresh: true,
         }),
         react(),
