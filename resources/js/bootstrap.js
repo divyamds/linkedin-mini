8cb05191a690bb8b6cc5d7c9ed724b78edf6e6ef
divyamds <55553324+divyamds@users.noreply.github.com>
2025-08-03 14:08:50 +0530

    Update bootstrap.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,6 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+if (import.meta.env.VITE_APP_URL?.startsWith('https')) {
+    window.axios.defaults.baseURL = import.meta.env.VITE_APP_URL;
+}
